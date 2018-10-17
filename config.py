add entropy regularization, add logits averaging, fix variable init bug in train(), fix image preprocessing bug

config.py
@@ -8,9 +8,9 @@ arg_lists = []
 parser = argparse.ArgumentParser()
 
 # Possible actions
-left = [1, 0, 0]
-right = [0, 1, 0]
-shoot = [0, 0, 1]
+shoot = [1, 0, 0]
+left = [0, 1, 0]
+right = [0, 0, 1]
 
 
 # ----------------------------------------
@@ -52,6 +52,10 @@ train_arg.add_argument("--freq", type=int,
                        default=10,
                        help="Frequency to decrease epsilon")
 
+train_arg.add_argument("--entropy_rate", type=int,
+                       default=1e-2,
+                       help="Ratio of entropy regularization to apply to loss")
+
 train_arg.add_argument("--log_dir", type=str,
                        default="./logs",
                        help="Directory to save logs and current model")
@@ -69,7 +73,7 @@ test_arg.add_argument("--test_episodes", type=int,
 model_arg = add_argument_group("Model")
 
 model_arg.add_argument("--actions", type=int,
-                       default=[left, right, shoot],
+                       default=[shoot, left, right],
                        help="Possible actions to take")
 
 model_arg.add_argument("--num_actions", type=int,
@@ -85,7 +89,7 @@ model_arg.add_argument("--activ",
                        help="Activation function to use")
 
 model_arg.add_argument("--init",
-                       default=tf.keras.initializers.he_normal(),
+                       default=tf.contrib.layers.xavier_initializer(),
                        help="Initialization function to use")
 
 # ----------------------------------------

main.py
@@ -42,8 +42,7 @@ class Q_Learning(object):
         screen = np.multiply(screen, 255.0/screen.max())
         #screen = tf.image.per_image_standardization(state.screen_buffer)
         screen = tf.image.rgb_to_grayscale(screen)
-        screen = tf.image.resize_images(screen, self.cfg.resolution)
-        self.frames.append(screen)
+        self.screen = tf.image.resize_images(screen, self.cfg.resolution)
 
     def logger(self):
         with tf.contrib.summary.record_summaries_every_n_global_steps(10, self.global_step):
@@ -52,6 +51,7 @@ class Q_Learning(object):
             tf.contrib.summary.scalar('reward', self.reward)
             tf.contrib.summary.scalar('action', self.idx)
             tf.contrib.summary.scalar('q-values', self.new_q)
+            tf.contrib.summary.scalar('entropy', self.entropy)
 
             # Log weights
             slots = self.optimizer.get_slot_names()
@@ -69,6 +69,9 @@ class Q_Learning(object):
             self.forward()
             self.reward = self.game.make_action(self.e_greedy(), self.cfg.skiprate)
             self.loss = tf.losses.mean_squared_error(self.reward + self.cfg.discount * self.new_q, self.old_q)
+            probs = tf.nn.softmax(self.logits)
+            self.entropy = -1 * tf.reduce_sum(probs*tf.math.log(probs))
+            self.loss -= self.entropy * self.cfg.entropy_rate
 
         self.logger()
         # Compute/apply gradients
@@ -86,7 +89,7 @@ class Q_Learning(object):
 
     def forward(self):
         # Get Q values for all actions
-        self.logits = self.model(self.frames)[0]
+        self.logits = tf.reduce_sum(self.model(self.frames), axis=0) / 4
         # Get highest Q value index
         self.idx = tf.argmax(self.logits, 0)
         self.new_q = self.logits[self.idx]
@@ -95,23 +98,27 @@ class Q_Learning(object):
         self.choice[self.idx] += 1
     
     def train(self):
-        self.old_q = 0
-        self.frames = []
         for i in trange(self.cfg.episodes):
             # Reduce exploration rate
             if i % self.cfg.freq == 0:
                 self.cfg.epsilon -= 0.1
 
+            # Setup variables
             self.game.new_episode()
+            self.old_q = 0
+            self.frames = []
+            self.preprocess()
+            for _ in range(3):
+                self.frames.append(self.screen)
+
             while not self.game.is_episode_finished():
                 self.preprocess()
-                # Ensure batch size is 4
-                if len(self.frames) == 4:
-                    self.update()
+                self.frames.append(self.screen)
+                self.update()
 
-                    #diagnostic(self.logits.numpy(), self.idx.numpy(), self.reward)
-                    # Remove oldest frame
-                    self.frames.pop(0)
+                #diagnostic(self.logits.numpy(), self.idx.numpy(), self.reward)
+                # Remove oldest frame
+                self.frames.pop(0)
 
     def test(self):
         self.old_q = 0